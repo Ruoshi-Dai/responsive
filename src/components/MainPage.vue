ddb8d89fc760710634dfc257fffd01744e42cf36
Ruoshi Dai <ruoshi@Ruoshis-MacBook-Pro.local>
2022-12-28 15:17:41 -0600

    add cube-ui

---
+++ src/components/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h2><cube-button> Test </cube-button></h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: blueviolet;
+}
+</style>
