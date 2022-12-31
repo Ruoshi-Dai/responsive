2cb94af6a50aa9e4943be0692c7c4a15e7a57fd9
Ruoshi Dai <rosalie.dai99@gmail.com>
2022-12-30 21:04:28 -0600

    checkbox and textarea added

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <h1
+      :style="{
+        fontSize: screenWidth/50 + 'px',
+      }">
+      1. Button:
+    </h1>
     <div class="wrapper">
       <cube-button
       id = "button1"
@@ -7,7 +13,7 @@
       :style="{
         height: minWH/5 + 'px',
         width: minWH/5 + 'px',
-        fontSize: minWH/25 + 'px'
+        fontSize: minWH/25 + 'px',
         }"
       :inline="true"
       @click="hide">
@@ -20,8 +26,61 @@
         The sides of the left button is 1/5 of the shorter side of the screen.
       </span>
     </div>
+    <h1
+      :style="{
+        fontSize: screenWidth/50 + 'px',
+      }">
+      2. Checkbox:
+    </h1>
     <div>
-      <cube-switch v-model="Switch">Switch</cube-switch>
+      <cube-checkbox
+        v-model="checked1"
+        :style="{
+          width: screenWidth/4 + 'px',
+          fontSize: screenWidth/60 + 'px',
+        }">
+        Checkbox 1
+      </cube-checkbox>
+      <cube-checkbox
+        v-model="checked2"
+        :style="{
+          width: screenWidth/4 + 'px',
+          fontSize: screenWidth/60 + 'px',
+        }">
+        Checkbox 2
+      </cube-checkbox>
+      <cube-checkbox
+        v-model="checked3"
+        :style="{
+          width: screenWidth/4 + 'px',
+          fontSize: screenWidth/60 + 'px',
+        }">
+        Checkbox 3
+      </cube-checkbox>
+    </div>
+    <h1
+      :style="{
+        fontSize: screenWidth/50 + 'px',
+      }">
+      3. Textarea:
+    </h1>
+    <div class="textareaEX">
+      <cube-textarea
+        v-model="txt1"
+        :style="{
+          width: screenWidth/3 + 'px',
+          height: 3*screenWidth/70 + 'px',
+          fontSize: screenWidth/70 + 'px',
+        }"></cube-textarea>
+      <span
+        class="textBox"
+        :style="{
+          width: screenWidth/3 -20 + 'px',
+          height: 3*screenWidth/70 -20 + 'px',
+          fontSize: screenWidth/70 + 'px',
+        }">
+        {{ txt1 }}
+      </span>
     </div>
   </div>
 </template>
@@ -32,10 +91,15 @@ export default {
   data () {
     return {
       minWH: Math.min(window.innerHeight, window.innerWidth),
+      screenWidth: window.innerWidth,
       msg: 'Hello World',
       buttonMsg: 'Hide',
       value: '',
-      Switch: true
+      Switch: true,
+      checked1: false,
+      checked2: false,
+      checked3: false,
+      txt1: ''
     }
   },
   methods: {
@@ -57,6 +121,7 @@ export default {
         window.screenHeight = window.innerHeight
         window.screenWidth = window.innerWidth
         that.minWH = Math.min(window.screenHeight, window.screenWidth)
+        that.screenWidth = window.screenWidth
       })()
     }
   },
@@ -71,12 +136,35 @@ export default {
           that.timer = false
         }, 400)
       }
+    },
+    screenWidth (val) {
+      if (!this.timer) {
+        this.screenWidth = val
+        this.timer = true
+        let that = this
+        setTimeout(function () {
+          console.log('screenWidth: ' + that.screenWidth)
+          that.timer = false
+        }, 400)
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+h1 {
+  color:blueviolet;
+  font-weight: bolder;
+  text-align: left;
+  margin-left: 5%;
+  margin-top: 5%;
+}
+
+.wrapper {
+  margin-top: 2%;
+}
+
 .c-button {
   text-align: center;
   padding-left: 5px;
@@ -97,8 +185,33 @@ export default {
   visibility: visible;
 }
 
-cube-switch {
-  border-style: solid;
-  background-color: aqua;
+.cube-checkbox {
+  background-color: #fbf2fb;
+  display: inline-block;
+  margin-top: 23px;
+  padding-left: 2%;
+  padding-right: 2%;
+}
+
+.textareaEX {
+  margin-bottom: 5%;
+  display: flex;
+}
+
+.cube-textarea-wrapper {
+  margin-top: 2%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+
+.textBox {
+  margin-top: 2%;
+  margin-left: 5%;
+  margin-right: 5%;
+  text-align: left;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-right: 10px;
+  padding-left: 10px;
 }
 </style>
