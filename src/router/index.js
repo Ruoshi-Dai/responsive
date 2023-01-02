585917d5b1b7bcf3f84e6336d84ab5ab1b817e80
Ruoshi Dai <rosalie.dai99@gmail.com>
2023-01-02 12:27:58 -0600

    album frame finished

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import MainPage from '@/components/MainPage'
+// import MainPage from '@/components/MainPage'
+import Album from '@/components/Album'
 
 Vue.use(Router)
 
@@ -9,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'MainPage',
-      component: MainPage
+      name: 'Album',
+      component: Album
     }
   ]
 })
