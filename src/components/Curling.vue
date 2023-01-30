0276c9debc11e841613b54a3d7e9ee44bdcf4291
Ruoshi Dai <rosalie.dai99@gmail.com>
2023-01-29 18:23:09 -0600

    modified the effect on mobile devices

--- src/components/Curling.vue
+++ src/components/Curling.vue
@@ -1,20 +1,24 @@
 <template>
-  <div id="curling" class="wrapper">
-    <div class="page leftpage" :style="{backgroundImage: 'url(' + items[imgind].image + ')'}"></div>
-    <div class="page rightpage"
-        :style="{backgroundImage: 'url(' + items[(imgind + 1) % items.length].image + ')'}"></div>
+  <div id="curling"
+    class="wrapper"
+    @mousemove="moveOutSide($event)"
+    @touchmove="moveOutSide($event)">
+    <div v-if="!isMobile" class="page leftpage" :style="{backgroundImage: 'url(' + items[imgind].image + ')'}"></div>
+    <div class="page rightpage" :style="rightPageStl"></div>
     <div class="turn-wrapper"
         :style="turnWrapperStl"
-        @mousedown="rightMouseDown($event)"
-        @mouseup="rightMouseUp($event)"
-        @mousemove="rightMouseMove($event)">
+        @mousedown="mouseDownHandler($event)"
+        @mouseup="mouseUpHandler($event)"
+        @mousemove="mouseMoveHandler($event)"
+        @touchstart="touchStartHandler($event)"
+        @touchmove="touchMoveHandler($event)"
+        @touchend="touchEndHandler($event)">
       <div class="turn-page" :style="turnPageStl"></div>
     </div>
     <div class="turn-wrapper2"
         :style="turnWrapper2Stl">
         <div class="turn-page2" :style="turnPage2Stl"></div>
     </div>
-    <div class="corner br" @mousedown="brMousedown"></div>
   </div>
 </template>
 
@@ -64,21 +68,31 @@ export default {
       transX: 0,
       transY: 0,
       wrapX: 0,
-      transitionT: 0,
       page1: '',
       page2: '',
       wrap1: '',
       wrap2: '',
-      imgind: 0
+      imgind: 0,
+      isMobile: navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
     }
   },
   computed: {
+    rightPageStl: function () {
+      if (this.isMobile) {
+        return {
+          width: '80%',
+          backgroundImage: 'url(' + this.items[this.imgind % this.items.length].image + ')'
+        }
+      } else {
+        return {
+          backgroundImage: 'url(' + this.items[(this.imgind + 1) % this.items.length].image + ')'
+        }
+      }
+    },
     turnWrapperStl: function () {
-      var w = this.screenWidth * 0.4
+      var w = this.isMobile ? this.screenWidth * 0.8 : this.screenWidth * 0.4
       var h = this.screenHeight * 0.8
       var boxL = Math.sqrt(w * w + h * h)
-      // var boxT = (this.screenHeight - boxL) / 2
-      // var y = this.screenHeight * 0.9 - boxT
       return {
         width: boxL + 'px',
         height: boxL + 'px',
@@ -91,7 +105,7 @@ export default {
       }
     },
     turnWrapper2Stl: function () {
-      var w = this.screenWidth * 0.4
+      var w = this.isMobile ? this.screenWidth * 0.8 : this.screenWidth * 0.4
       var h = this.screenHeight * 0.8
       var boxL = Math.sqrt(w * w + h * h)
       // var boxT = (this.screenHeight - boxL) / 2
@@ -108,7 +122,7 @@ export default {
       }
     },
     turnPageStl: function () {
-      var w = this.screenWidth * 0.4
+      var w = this.isMobile ? this.screenWidth * 0.8 : this.screenWidth * 0.4
       var h = this.screenHeight * 0.8
       // var boxL = Math.sqrt(w * w + h * h)
       return {
@@ -120,11 +134,13 @@ export default {
         right: 'auto',
         transform: 'translate(' + this.transX + 'px, ' + this.transY + 'px) rotate(' + this.rightdeg + 'deg)',
         transformOrigin: '0% 100%',
-        backgroundImage: 'url(' + this.items[(this.imgind + 2) % this.items.length].image + ')'
+        backgroundColor: this.isMobile ? 'white' : '',
+        backgroundImage: this.isMobile ? '' : 'url(' + this.items[(this.imgind + 2) % this.items.length].image + ')'
+        // backgroundColor: 'white'
       }
     },
     turnPage2Stl: function () {
-      var w = this.screenWidth * 0.4
+      var w = this.isMobile ? this.screenWidth * 0.8 : this.screenWidth * 0.4
       var h = this.screenHeight * 0.8
       // var boxL = Math.sqrt(w * w + h * h)
       var deg = 0 - this.rightdeg
@@ -138,7 +154,7 @@ export default {
         left: 'auto',
         transform: 'translate(' + dx + 'px, ' + this.transY + 'px) rotate(' + deg + 'deg)',
         transformOrigin: '100% 100%',
-        backgroundImage: 'url(' + this.items[(this.imgind + 3) % this.items.length].image + ')'
+        backgroundImage: 'url(' + this.items[(this.isMobile ? this.imgind + 1 : this.imgind + 3) % this.items.length].image + ')'
       }
     }
   },
@@ -161,104 +177,186 @@ export default {
       that.rightdeg = alpha / Math.PI * 180
       // console.log((x2 - x1) * (x2 - x1) + (y2 - y) * (y2 - y), (y1 - y) * (y1 - y), y2, (x0 - x1) / (y1 - y0) * x2 + (y0 + y1) / 2 - (x0 * x0 - x1 * x1) / (2 * (y1 - y0)))
     },
-    brMousedown: function () {
-      console.log('Mouse down on br')
+    turningAnimationPhase1 (that) {
+      if (that.moveEventFlag === true) {
+        // console.log('x: ' + e.x + ' y: ' + e.y)
+        if (!that.page1) {
+          that.page1 = document.querySelector('.turn-page')
+          that.page2 = document.querySelector('.turn-page2')
+          that.wrap1 = document.querySelector('.turn-wrapper')
+          that.wrap2 = document.querySelector('.turn-wrapper2')
+        }
+        var alpha = that.rightdeg * Math.PI / 180
+        var newtransX1 = 0 - that.screenWidth * (that.isMobile ? 0.8 : 0.4) * Math.cos(alpha)
+        var newtransY1 = 0 - that.screenWidth * (that.isMobile ? 0.8 : 0.4) * Math.sin(alpha)
+        var old2x = 0 - that.transX
+        var old2deg = 0 - that.rightdeg
+        var new2x1 = 0 - newtransX1
+        var newWrapX = 0 - that.screenWidth * (that.isMobile ? 0.8 : 0.4)
+        const page1Move = [
+          {transform: 'translate(' + that.transX + 'px, ' + that.transY + 'px) rotate(' + that.rightdeg + 'deg)'},
+          {transform: 'translate(' + newtransX1 + 'px, ' + newtransY1 + 'px) rotate(' + that.rightdeg + 'deg)'}
+        ]
+        const page2Move = [
+          {transform: 'translate(' + old2x + 'px, ' + that.transY + 'px) rotate(' + old2deg + 'deg)'},
+          {transform: 'translate(' + new2x1 + 'px, ' + newtransY1 + 'px) rotate(' + old2deg + 'deg)'}
+        ]
+        const wrap1Move = [
+          {transform: 'translateX(' + that.wrapX + 'px) rotate(' + that.rightdeg + 'deg)'},
+          {transform: 'translateX(' + newWrapX + 'px) rotate(' + that.rightdeg + 'deg)'}
+        ]
+        const wrap2Move = [
+          {transform: 'translateX(' + that.wrapX + 'px) rotate(' + that.rightdeg + 'deg)'},
+          {transform: 'translateX(' + newWrapX + 'px) rotate(' + that.rightdeg + 'deg)'}
+        ]
+        const duration1 = {duration: 1000}
+        that.page1.animate(page1Move, duration1)
+        that.page2.animate(page2Move, duration1)
+        that.wrap1.animate(wrap1Move, duration1)
+        that.wrap2.animate(wrap2Move, duration1)
+      }
     },
-    rightMouseDown (e) {
-      this.transitionT = 0
+    turningAnimationPhase2 (that) {
+      if (that.moveEventFlag === true) {
+        that.moveEventFlag = false
+        if (!that.page1) {
+          that.page1 = document.querySelector('.turn-page')
+          that.page2 = document.querySelector('.turn-page2')
+          that.wrap1 = document.querySelector('.turn-wrapper')
+          that.wrap2 = document.querySelector('.turn-wrapper2')
+        }
+        var old2deg = 0 - that.rightdeg
+        var newWrapX = 0 - that.screenWidth * (that.isMobile ? 0.8 : 0.4)
+        var newtransX2 = 0 - that.screenWidth * (that.isMobile ? 0.8 : 0.4)
+        var newtransY2 = 0
+        var new2x2 = 0 - newtransX2
+        console.log('rightdegree: ' + that.rightdeg)
+        const page1Rotate = [
+          {transform: 'translate(' + newtransX2 + 'px, ' + newtransY2 + 'px) rotate(' + that.rightdeg + 'deg)', transformOrigin: '100% 100%'},
+          {transform: 'translate(' + newtransX2 + 'px, ' + newtransY2 + 'px) rotate(0deg)', transformOrigin: '100% 100%'}
+        ]
+        const page2Rotate = [
+          {transform: 'translate(' + new2x2 + 'px, ' + newtransY2 + 'px) rotate(' + old2deg + 'deg)', transformOrigin: '0% 100%'},
+          {transform: 'translate(' + new2x2 + 'px, ' + newtransY2 + 'px) rotate(0deg)', transformOrigin: '0% 100%'}
+        ]
+        const wrap1Rotate = [
+          {transform: 'translateX(' + newWrapX + 'px) rotate(' + that.rightdeg + 'deg)'},
+          {transform: 'translateX(' + newWrapX + 'px) rotate(0deg)'}
+        ]
+        const wrap2Rotate = [
+          {transform: 'translateX(' + newWrapX + 'px) rotate(' + that.rightdeg + 'deg)'},
+          {transform: 'translateX(' + newWrapX + 'px) rotate(0deg)'}
+        ]
+        var duration2 = {duration: 1000, delay: 1000}
+        var myanimation = that.page1.animate(page1Rotate, duration2)
+        that.page2.animate(page2Rotate, duration2)
+        that.wrap1.animate(wrap1Rotate, duration2)
+        that.wrap2.animate(wrap2Rotate, duration2)
+        myanimation.onfinish = (event) => {
+          that.transX = 0
+          that.transY = 0
+          that.wrapX = 0
+          that.rightdeg = 0
+          that.imgind = (that.imgind + (that.isMobile ? 1 : 2)) % that.items.length
+        }
+      }
+    },
+    outofRange (that, e) {
+      var x1 = that.screenWidth * 0.9
+      var y1 = that.screenHeight * 0.9
+      var x0, y0, x
+      if (that.isMobile) {
+        x0 = e.touches[0].clientX
+        y0 = e.touches[0].clientY
+        x = (y1 - y0) / (x0 - x1) * ((y1 - y0) / 2) + (x0 + x1) / 2
+        if (x <= that.screenWidth * 0.1) {
+          return true
+        }
+      } else {
+        x0 = e.x
+        y0 = e.y
+        x = (y1 - y0) / (x0 - x1) * ((y1 - y0) / 2) + (x0 + x1) / 2
+        if (x <= that.screenWidth * 0.5) {
+          return true
+        }
+      }
+      return false
+    },
+    moveOutSide (e) {
+      if (this.moveEventFlag) {
+        console.log('move outside')
+        var x = this.isMobile ? e.touches[0].clientX : e.x
+        var y = this.isMobile ? e.touches[0].clientY : e.y
+        if (x >= this.screenWidth * 0.9 || x <= this.screenWidth * 0.1 ||
+          y >= this.screenHeight * 0.9 || y <= this.screenHeight * 0.1) {
+          this.$options.methods.turningAnimationPhase1(this)
+          this.$options.methods.turningAnimationPhase2(this)
+        }
+      }
+    },
+    mouseDownHandler (e) {
+      if (this.isMobile) return
       var brX = this.screenWidth * 0.9
       var brY = this.screenHeight * 0.9
-      if (e.x <= brX + this.screenWidth * 0.05 && e.x >= brX - this.screenWidth * 0.05 &&
-          e.y <= brY + this.screenHeight * 0.05 && e.y >= brY - this.screenHeight * 0.05) {
-        console.log('mousedown in corner')
+      console.log(e.x, e.y)
+      if (e.x <= brX && e.x >= brX - this.screenWidth * 0.1 &&
+          e.y <= brY && e.y >= brY - this.screenHeight * 0.1) {
+        console.log(this.isMobile ? 'touch start in corner' : 'mousedown in corner')
         this.moveEventFlag = true
-        this.shadowX = 0 - this.screenWidth * 0.01
-        this.shadowY = this.screenHeight * 0.01
-        this.shadowV = this.screenHeight * 0.01
         this.$options.methods.computeDeg(this, e.x, e.y)
       } else {
-        console.log('mousedown not in corner')
+        console.log(this.isMobile ? 'touch start not in corner' : 'mousedown not in corner')
       }
     },
-    rightMouseUp (e) {
-      console.log('x: ' + e.x + ' y: ' + e.y)
-      this.moveEventFlag = false
-      if (!this.page1) {
-        this.page1 = document.querySelector('.turn-page')
-        this.page2 = document.querySelector('.turn-page2')
-        this.wrap1 = document.querySelector('.turn-wrapper')
-        this.wrap2 = document.querySelector('.turn-wrapper2')
-      }
-      var alpha = this.rightdeg * Math.PI / 180
-      var newtransX1 = 0 - this.screenWidth * 0.4 * Math.cos(alpha)
-      var newtransY1 = 0 - this.screenWidth * 0.4 * Math.sin(alpha)
-      var old2x = 0 - this.transX
-      var old2deg = 0 - this.rightdeg
-      var new2x1 = 0 - newtransX1
-      var newWrapX = 0 - this.screenWidth * 0.4
-      const page1Move = [
-        {transform: 'translate(' + this.transX + 'px, ' + this.transY + 'px) rotate(' + this.rightdeg + 'deg)'},
-        {transform: 'translate(' + newtransX1 + 'px, ' + newtransY1 + 'px) rotate(' + this.rightdeg + 'deg)'}
-      ]
-      const page2Move = [
-        {transform: 'translate(' + old2x + 'px, ' + this.transY + 'px) rotate(' + old2deg + 'deg)'},
-        {transform: 'translate(' + new2x1 + 'px, ' + newtransY1 + 'px) rotate(' + old2deg + 'deg)'}
-      ]
-      const wrap1Move = [
-        {transform: 'translateX(' + this.wrapX + 'px) rotate(' + this.rightdeg + 'deg)'},
-        {transform: 'translateX(' + newWrapX + 'px) rotate(' + this.rightdeg + 'deg)'}
-      ]
-      const wrap2Move = [
-        {transform: 'translateX(' + this.wrapX + 'px) rotate(' + this.rightdeg + 'deg)'},
-        {transform: 'translateX(' + newWrapX + 'px) rotate(' + this.rightdeg + 'deg)'}
-      ]
-      const duration1 = {duration: 1000}
-      this.page1.animate(page1Move, duration1)
-      this.page2.animate(page2Move, duration1)
-      this.wrap1.animate(wrap1Move, duration1)
-      this.wrap2.animate(wrap2Move, duration1)
-      var newtransX2 = 0 - this.screenWidth * 0.4
-      var newtransY2 = 0
-      var new2x2 = 0 - newtransX2
-      const page1Rotate = [
-        {transform: 'translate(' + newtransX2 + 'px, ' + newtransY2 + 'px) rotate(' + this.rightdeg + 'deg)', transformOrigin: '100% 100%'},
-        {transform: 'translate(' + newtransX2 + 'px, ' + newtransY2 + 'px) rotate(0deg)', transformOrigin: '100% 100%'}
-      ]
-      const page2Rotate = [
-        {transform: 'translate(' + new2x2 + 'px, ' + newtransY2 + 'px) rotate(' + old2deg + 'deg)', transformOrigin: '0% 100%'},
-        {transform: 'translate(' + new2x2 + 'px, ' + newtransY2 + 'px) rotate(0deg)', transformOrigin: '0% 100%'}
-      ]
-      const wrap1Rotate = [
-        {transform: 'translateX(' + newWrapX + 'px) rotate(' + this.rightdeg + 'deg)'},
-        {transform: 'translateX(' + newWrapX + 'px) rotate(0deg)'}
-      ]
-      const wrap2Rotate = [
-        {transform: 'translateX(' + newWrapX + 'px) rotate(' + this.rightdeg + 'deg)'},
-        {transform: 'translateX(' + newWrapX + 'px) rotate(0deg)'}
-      ]
-      var duration2 = {duration: 1000, delay: 1000}
-      var myanimation = this.page1.animate(page1Rotate, duration2)
-      this.page2.animate(page2Rotate, duration2)
-      this.wrap1.animate(wrap1Rotate, duration2)
-      this.wrap2.animate(wrap2Rotate, duration2)
-      this.transX = 0
-      this.transY = 0
-      this.wrapX = 0
-      this.rightdeg = 0
-      myanimation.onfinish = (event) => {
-        this.imgind = (this.imgind + 2) % this.items.length
-      }
-      // this.imgind = this.imgind + 2
-      // console.log('newtransX: ' + this.transX)
+    mouseUpHandler (e) {
+      if (this.isMobile) return
+      this.$options.methods.turningAnimationPhase1(this)
+      this.$options.methods.turningAnimationPhase2(this)
     },
-    rightMouseMove (e) {
+    mouseMoveHandler (e) {
+      if (this.isMobile) return
       if (this.moveEventFlag) {
+        if (this.$options.methods.outofRange(this, e)) {
+          this.$options.methods.turningAnimationPhase2(this)
+          return
+        }
         this.$options.methods.computeDeg(this, e.x, e.y)
       }
+    },
+    touchStartHandler (e) {
+      // console.log('touch start at', e.touches[0].clientX, e.touches[0].clientY)
+      if (!this.isMobile) return
+      var brX = this.screenWidth * 0.9
+      var brY = this.screenHeight * 0.9
+      var x = e.touches[0].clientX
+      var y = e.touches[0].clientY
+      console.log(e.x, e.y)
+      if (x <= brX && x >= brX - this.screenWidth * 0.1 &&
+          y <= brY && y >= brY - this.screenHeight * 0.1) {
+        console.log(this.isMobile ? 'touch start in corner' : 'mousedown in corner')
+        this.moveEventFlag = true
+        this.$options.methods.computeDeg(this, x, y)
+      } else {
+        console.log(this.isMobile ? 'touch start not in corner' : 'mousedown not in corner')
+      }
+    },
+    touchMoveHandler (e) {
+      if (!this.isMobile) return
+      if (this.moveEventFlag) {
+        if (this.$options.methods.outofRange(this, e)) {
+          this.$options.methods.turningAnimationPhase2(this)
+          return
+        }
+        this.$options.methods.computeDeg(this, e.touches[0].clientX, e.touches[0].clientY)
+      }
+    },
+    touchEndHandler (e) {
+      if (!this.isMobile) return
+      this.$options.methods.turningAnimationPhase1(this)
+      this.$options.methods.turningAnimationPhase2(this)
     }
   },
   mounted () {
-    // console.log(window.innerWidth, window.innerHeight)
     const that = this
     window.onresize = () => {
       return (() => {
